6a06648b1526502b55d7003b09a84b28e17b4829
Jonathan Gaudreau <jgaudreau@esri.ca>
2018-01-12 16:53:44 -0500

    Ajout du code initial

---
+++ ElevationProfile/nls/fr/strings.js
@@ -0,0 +1,32 @@
+define({
+      "descriptionlabel": "Utilisez l'outil Mesurer pour tracer une ligne sur la carte pour laquelle vous souhaitez afficher le profil d'élévation.",
+      "chartLabel": "Passez la souris sur ou cliquez sur le graphique Profil d'altitude pour afficher les altitudes et afficher l'emplacement sur la carte.",
+      "clear": "Supprimer",
+      "prepare": "Préparer le téléchargement...",
+      "download": "Télécharger",
+      "measurelabel": "Mesure",
+      "resultslabel": "Résultat du profil",
+      "profileinfo": "Information du profil",
+      "display": {
+        "elevationProfileTitle": "Profil d'élévation",
+        "hoverOver": "Passez la souris sur ou cliquez sur le graphique Profil d'altitude pour afficher les altitudes et afficher l'emplacement sur la carte."
+      },
+      "chart": {
+        "title": "Profil d'élévation",
+        "demResolution": "Résolution du DEM",
+        "elevationTitleTemplate": "Élévation en {0}",
+        "distanceTitleTemplate": "Distance en {0}",
+        "gainLossTemplate": "Élévation Min: {min} {unit}<br>Élévation Max: {max} {unit}<br>Élévation Départ: {start} {unit}<br>Élévation Fin: {end} {unit}<br>Dénivelé: {gainloss} {unit}"
+      },
+      "errors": {
+        "InvalidConfiguration": "Configuration invalide.",
+        "UnableToProcessResults": "Incapable de traiter les résultats."
+      },
+      "widgetversion": "Informations sur la version du widget de profil d'élévation",
+      "widgetverstr": 'Version du Widget',
+      "wabversionmsg": 'Le Widget est conçu pour être utilisé dans Web AppBuilder',
+      "resultactions": "Menu des résultats",
+      "_featureAction_exportToCSV": "Exporter le profil vers un CSV",
+      "flipProfile": "Inverser la direction du profil"
+  });
+  
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ProfilElevation
+Ce Widget a été créé par Robert Scheitlin, mais n'a pas encore été traduit.
+J'ai donc créé une version française (je n'ai que modifié le dossier NLS et ajouté la langue française).
+
+Pour l'utiliser, déposer les fichiers dans WebAppBuilder/client/stemapp/widgets et vous pourrez l'utiliser.
